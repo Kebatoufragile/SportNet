2b981e5e27db1e31e23ee4314061a404136ddab2
giovanne3u <giovannelli.alexis@gmail.com>
2016-11-16 16:45:14 +0100

    [WIP] add Participant

---
+++ app/src/Controllers/ParticipantController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\controllers;
+
+use App\Models\Participant;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class ParticipantController extends AbstractController{
+
+  protected $view;
+  protected $logger;
+  protected $sentinel;
+
+  public function __construct($view){
+    parent::__construct($view);
+  }
+
+  public function addToEvent(Request $request, Response $response, $args ){
+    
+  }
+
+}
