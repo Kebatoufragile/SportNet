d877dae8dab238e9250b4ac87aeb4b96a2bb0d5e
giovanne3u <giovannelli.alexis@gmail.com>
2016-11-16 16:45:14 +0100

    [WIP] add Participant

---
+++ app/src/Models/Participant.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Participant extends Model
+{
+  protected $table = "participant";
+  public $timestamps = false;
+}
